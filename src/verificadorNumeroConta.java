b1dad2181e8f8b4243b67448f020f89988cbe86e
Nid silva <80358224+Ivoneide01@users.noreply.github.com>
2024-05-04 01:05:30 -0300

    atualizando

--- src/verificadorNumeroConta.java
+++ src/verificadorNumeroConta.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class VerificadorNumeroConta {
+public class verificadorNumeroConta { // esta em letra minuscula
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
